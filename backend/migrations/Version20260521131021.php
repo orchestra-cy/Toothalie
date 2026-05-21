a2e5f079ba16699b4e941787c52933175e2b2e31
cl1ntdev <clintjayestrellanes17@gmail.com>
2026-05-21 16:02:45 +0800

    feat: Insert data

--- backend/migrations/Version20260521131021.php
+++ backend/migrations/Version20260521131021.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260521131020 extends AbstractMigration
+final class Version20260521131021 extends AbstractMigration
 {
     public function getDescription(): string
     {
